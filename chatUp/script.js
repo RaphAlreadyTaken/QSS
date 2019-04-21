b1cb28c464e3e9c12eb13da2d1788147e190d39a
Casanova Delfeil <uapv1602054@pedago02a.univ-avignon.fr>
2019-04-21 16:17:23 +0200

    js add comments...

--- chatUp/script.js
+++ chatUp/script.js
@@ -1,5 +1,8 @@
 (function(){
 
+  /**
+   * Chargement du css associé au module
+   */
   var link = document.createElement("link");
   link.href = 'http://localhost:3101/embedCss';
   // link.href = 'http://pedago01c.univ-avignon.fr:3101/embedCss';
@@ -7,16 +10,26 @@
   link.type = 'text/css';
   document.getElementsByTagName("head")[0].appendChild(link);
 
+  /**
+   * Chergement du websocket
+   */
   var script = document.createElement("SCRIPT");
   script.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js';
   script.type = 'text/javascript';
   document.getElementsByTagName("head")[0].appendChild(script);
 
-  window.chatUp = {
+
+  /**
+   * Classe ChatUp: Module de chat
+   */
+  window.ChatUp = {
     conf: {},
     socket: null,
+    /**
+     * Fonction construisant l'outil de chat
+     */
     load: function() {
-      this.conf.elem = $('#'+this.conf.elem_id);
+      this.conf.elem = $('#'+this.conf.elemId);
       this.conf.elem.html(
         '<div id="chatup-form">'+
           '<div id="chatup-messages">'+
@@ -40,37 +53,33 @@
         '</div>'
       );
     },
-    init: function(elem_id, color) {
-      console.log("Chat_up initialisation", elem_id, color);
+    /**
+     * Fonction initialisant le module
+     * param: elemId: emplacement où sera placé le module, dans la page html
+     * param: color: Couleur personnalisable, de l'outil
+     */
+    init: function(elemId, color) {
+      console.log("Chat_up initialisation", elemId, color);
       var conf = {};
-      conf.elem_id = elem_id;
+      conf.elemId = elemId;
       if(typeof color !== "undefined") {
         conf.color = color;
       } else {
         conf.color = '#6c757d';
       }
       document.documentElement.style.setProperty('--chatup-color', conf.color);
-      window.chatUp.conf = Object.assign(conf, window.chatUp.conf);
-    }
-  }
+      window.ChatUp.conf = Object.assign(conf, window.ChatUp.conf);
+    },
+    /**
+     * Fonction Attribuant différens événements aux éléments constituants l'outil de chat
+     */
+    event: function() {
+      var self = this;
 
-  if(typeof window.io !== 'function') {
-    window.chatUp.socket = new WebSocket("ws://localhost:3101/chat");
-    // window.chatUp.socket = new WebSocket("ws://pedago01c.univ-avignon.fr:3101/chat");
-    document.getElementsByTagName("head")[0].appendChild(script);
-    window.chatUp.userId = 'Toto'; //TODO: modifier ça en formulaire de saisie de nom
-    window.chatUp.userId += '_' + Math.random().toString(36).substr(2, 5);  //Taken from https://gist.github.com/gordonbrander/2230317
-  }
-
-  //load jquery
-  if(typeof window.jQuery !== 'function') {
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
-    script.type = 'text/javascript';
-    script.onload = function() {
-      window.chatUp.load();
-
-      $(window.chatUp.socket).on('message', function(e)
+      /**
+       * Evénement de réception d'un message
+       */
+      $(self.socket).on('message', function(e)
       {
         msg = JSON.parse(e.originalEvent.data);
         console.log("Message received: " + msg.message);
@@ -79,45 +88,81 @@
         );
       });
 
+      /**
+       * Evénement d'envoie de message
+       */
       $('#form-chatup').on('submit', function(e) {
         e.preventDefault();
-        var $textarea = $('#textarea-chatup');
-        console.log("textArea: ", $textarea, $textarea.val());
-        if($textarea.val() == "")
+        var $textArea = $('#textarea-chatup');
+        console.log("textArea: ", $textArea, $textArea.val());
+        if($textArea.val() == "")
           return;
-        window.chatUp.socket.send(JSON.stringify({'message': $textarea.val(), 'id': window.chatUp.userId}));
+        self.socket.send(JSON.stringify({'message': $textArea.val(), 'id': window.ChatUp.userId}));
         $('#chatup-messages-list').append(
-          '<div class="chatup-user-msg">' + $textarea.val() + '</div>'
+          '<div class="chatup-user-msg">' + $textArea.val() + '</div>'
         );
-        $textarea.val('');
+        $textArea.val('');
       });
 
+      /**
+       * Evénement d'envoie de l'identifiant de l'utilisateur
+       */
       $('#form-chatup-id').on('submit', function(e) {
         e.preventDefault();
-        var $textarea = $('#textarea-chatup-id');
-        var pseudo = $textarea.val();
-        console.log("pseudo: ", $textarea, $textarea.val());
+        var $textArea = $('#textarea-chatup-id');
+        var pseudo = $textArea.val();
+        console.log("pseudo: ", $textArea, $textArea.val());
         if(pseudo == "")
           return;
-        window.chatUp.userId = pseudo; //TODO: modifier ça en formulaire de saisie de nom
-        window.chatUp.userId += '_' + Math.random().toString(36).substr(2, 5);  //Taken from https://gist.github.com/gordonbrander/2230317
-        window.chatUp.socket.send(JSON.stringify({'id': window.chatUp.userId}));
+        self.userId = pseudo; //TODO: modifier ça en formulaire de saisie de nom
+        self.userId += '_' + Math.random().toString(36).substr(2, 5);  //Taken from https://gist.github.com/gordonbrander/2230317
+        self.socket.send(JSON.stringify({'id': window.ChatUp.userId}));
         $('#form-chatup-id').addClass('hide');
         $('#mgs-need-id').addClass('hide');
         $('#form-chatup').removeClass('hide');
         $('#mgs-need-help').removeClass('hide');
         $('#chatup-pseudo').text(pseudo);
-        $textarea.val('');
+        $textArea.val('');
       });
 
+      /**
+       * Evéement de click pour cacher le chat
+       */
       $('.hide-chatup-msg').on('click', function(e) {
-        $chatup_messages = $('#chatup-messages-list');
-        $chatup_messages.toggleClass('hidden-msg');
+        $chatupMessages = $('#chatup-messages-list');
+        $chatupMessages.toggleClass('hidden-msg');
       });
+    }
+  }
+
+  /**
+   * Attribution du websocket
+   */
+  if(typeof window.io !== 'function') {
+    window.ChatUp.socket = new WebSocket("ws://localhost:3101/chat");
+    // window.ChatUp.socket = new WebSocket("ws://pedago01c.univ-avignon.fr:3101/chat");
+    document.getElementsByTagName("head")[0].appendChild(script);
+    window.ChatUp.userId = 'Toto'; //TODO: modifier ça en formulaire de saisie de nom
+    window.ChatUp.userId += '_' + Math.random().toString(36).substr(2, 5);  //Taken from https://gist.github.com/gordonbrander/2230317
+  }
+
+  //load jquery
+  if(typeof window.jQuery !== 'function') {
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+    script.type = 'text/javascript';
+
+    /**
+     * Fonction de callback, charge le module et cherge ses événements une fois que jQuery est importé
+     */
+    script.onload = function() {
+      window.ChatUp.load();
+      window.ChatUp.event();
     };
     document.getElementsByTagName("head")[0].appendChild(script);
   } else {
-    window.chatUp.load();
+    window.ChatUp.load();
+    window.ChatUp.event();
   }
 
 })();
